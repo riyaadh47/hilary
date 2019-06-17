0f29662864cba8d24e508c5c031631dc5e9d2f32
riyaadh47 <rfakier11@gmail.com>
2019-06-17 20:16:25 +0200

    Added Mosaic Style for Gallery

--- js/main.js
+++ js/main.js
@@ -13,8 +13,27 @@ $(document).ready(function(){
   });
 
   $("#gallery").nanogallery2({
-    thumbnailHeight: "auto",
-    thumbnailWidth: 300
+    "thumbnailWidth":           300,
+    "thumbnailHeight":          300,
+    "galleryMosaic": [
+      { w: 2, h: 2, c: 1, r: 1 },
+      { w: 1, h: 1, c: 3, r: 1 },
+      { w: 1, h: 1, c: 3, r: 2 },
+      { w: 1, h: 2, c: 4, r: 1 },
+      { w: 2, h: 1, c: 5, r: 1 },
+      { w: 2, h: 2, c: 5, r: 2 },
+      { w: 1, h: 1, c: 4, r: 3 },
+      { w: 2, h: 1, c: 2, r: 3 },
+      { w: 1, h: 2, c: 1, r: 3 },
+      { w: 1, h: 1, c: 2, r: 4 },
+      { w: 2, h: 1, c: 3, r: 4 },
+      { w: 1, h: 1, c: 5, r: 4 },
+      { w: 1, h: 1, c: 6, r: 4 }
+    ],
+    "thumbnailDisplayTransition":          "slideRight",
+    "thumbnailAlignment" : "justified",
+    "thumbnailDisplayTransitionDuration":  500,  
+    "thumbnailDisplayInterval":            30   
   });
 
 });
